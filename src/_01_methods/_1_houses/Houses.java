68da5f7c55afebad94d46267dadc38a37d654d2a
jereycui2008 <jeremycuid@gmail.com>
2023-11-13 16:44:05 -0800

    I did Houses.

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,4 +1,7 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -7,5 +10,32 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
+		Robot rob = new Robot();
+		for (int i=0; i<5; i++) {
+			rob.miniaturize();
+			rob.setSpeed(100);
+			rob.setPenColor(250,0,0);
+			rob.penDown();
+			rob.move(100);
+			rob.turn(50);
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
