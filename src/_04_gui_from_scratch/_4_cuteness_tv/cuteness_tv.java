9e1636a55a600b7e684eb7be0dcd884fe0671509
lvl0_module2 <zoey.lestyk@gmail.com>
2024-05-20 16:14:12 -0700

    I did cuteness_tv!!

--- src/_04_gui_from_scratch/_4_cuteness_tv/cuteness_tv.java
+++ src/_04_gui_from_scratch/_4_cuteness_tv/cuteness_tv.java
@@ -1,5 +1,93 @@
 package _04_gui_from_scratch._4_cuteness_tv;
 
-public class cuteness_tv {
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class cuteness_tv implements ActionListener {
+
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	JButton button = new JButton();
+    JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	
+	
+	
+	
+	
+	
+public void showButton() {
+        
+        frame.setVisible(true);
+        button.setText("video 1");
+        button2.setText("video 2");
+        button3.setText("video 3");        
+        panel.add(button);
+        panel.add(button2);
+        panel.add(button3);
+        frame.add(panel);
+        frame.pack();
+        button.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
+	}
+
+
+
+
+
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	JButton buttonPressed = (JButton) arg0.getSource();
+	if (buttonPressed.equals(button)) {
+		 playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+	if (buttonPressed.equals(button2)) {
+		playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+	if (buttonPressed.equals(button3)) {
+		playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+}
+	
+void playVideo(String videoID) {
+    
+    // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't
+    //work on some Linux implementations
+    try {
+    if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+    if (Runtime.getRuntime().exec(new String[] {
+     "which", "xdg- open" }).getInputStream().read() != -1) {
+    Runtime.getRuntime().exec(new String[] { "xdg-open", videoID });
+    }
+    } else {
+         URI uri = new URI(videoID);
+         java.awt.Desktop.getDesktop().browse(uri);
+    }      } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/_04_gui_from_scratch/_4_cuteness_tv/cuteness_tv_runner.java
+++ src/_04_gui_from_scratch/_4_cuteness_tv/cuteness_tv_runner.java
@@ -3,6 +3,9 @@ package _04_gui_from_scratch._4_cuteness_tv;
 public class cuteness_tv_runner {
 
 	public static void main(String[] args) {
+	cuteness_tv tv = new cuteness_tv();
+	tv.showButton();
+	
 	
 	}
 }
